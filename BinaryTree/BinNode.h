c9c08b1855c954002cb86c5ac51d95bddc063188
zhuhuiming <1294206499@qq.com>
2019-07-04 19:15:11 +0800

    完成了二叉树数据结构, 没有进行测试

--- BinaryTree/BinNode.h
+++ BinaryTree/BinNode.h
@@ -10,6 +10,21 @@
 #include "../queue/queue.h"
 #include "../stack/mystack.h"
 
+// 与BinNode状态与性质的判断
+#define IsRoot(x) (!((x).parent))
+#define IsLChild(x) (!IsRoot(x) && (&(x) == (x).parent->lc))
+#define IsRChild(x) (!IsRoot(x) && (&(x) == (x).parent->rc))
+#define HasParent(x) (!IsRoot(x))
+#define HasLChild(x) ((x).lc)
+#define HasRChild(x) ((x).rc)
+#define HasChild(x) (HasLChild(x) || HasRChild(x))
+#define HasBothChild(x) (HasLChild(x) && HasRChild(x))
+#define IsLeadf(x) (!HasChild(x))
+// 与BinNode具有特定关系的节点及指针
+#define sibling(p) (IsLChild(*(p)) ? (p)->parent->rc : (p)->parent->lc)
+#define uncle(x) (IsLChild(*((x)->parent)) ? (x)->parent->parent->rc : (x)->parent->parent->lc)
+#define FromParentTo(x) (IsRoot(x) ? _root :(IsLChild(x) ? (x).parent->lc : (x).parent.rc))
+
 #define BinNodePosi(T) BinNode<T>* //节点位置
 #define stature(p) ((p) ? (p)->height : -1) //节点高度（与“空树高度为-1”的约定相统一）
 typedef enum { RB_RED, RB_BLACK} RBColor; //节点颜色
@@ -35,8 +50,12 @@ template <typename T> struct BinNode { //二叉树节点模板类
    void alongLeftBranch(BinNode<T> *p, Stack<BinNode<T> *> S);
    template <typename VST> void travLevel ( VST& ); //子树层次遍历
    template <typename VST> void travPre ( VST& ); //子树先序遍历
+   template <typename VST> void traPre_I(VST &visit);
    template <typename VST> void travIn ( VST& ); //子树中序遍历
-   template <typename VST> void travPost ( VST& ); //子树后序遍历
+   template <typename VST> void traIn_I(VST &visit);
+   template <typename VST> void tranIn_I1(VST &visit);
+   template <typename VST> void travPost ( VST &visit); //子树后序遍历
+    template <typename VST> void travPost_I(VST &visit);
 // 比较器、判等器（各列其一，其余自行补充）
    bool operator< ( BinNode const& bn ) { return data < bn.data; } //小于
    bool operator== ( BinNode const& bn ) { return data == bn.data; } //等于
@@ -67,8 +86,10 @@ template <typename T> BinNode<T> * BinNode<T>::succ() {
     auto p = rc;
     if(p){
         while(p->lc)p = p->lc;
-    }else
-        p = parent;
+    }else {
+        while(IsRChild(*p))p = p->parent; // 是父亲的右孩子叶子节点
+        p = p->parent;
+    }
     return p;
 }
 template <typename T> template<typename VST> void BinNode<T>::travLevel(VST &visit) {
@@ -87,22 +108,77 @@ template <typename T> template <typename VST> void BinNode<T>::travPre(VST &visi
     travPre(this->lc);
     travPre(this->rc);
 }
+template <typename T> template <typename VST> void BinNode<T>::traPre_I(VST &visit) {
+    if(NULL == this)
+        return;
+    Stack<BinNode<T> *> S;
+    S.push(this);
+    while (!S.empty()){
+        auto p = S.pop();
+        visit(p->data);
+        if(HasRChild(*p))S.push(p->rc);
+        if(HasLChild(*p))S.push(p->lc);
+    }
+}
 template <typename T> template <typename VST> void BinNode<T>::travIn(VST &visit) { // 递归实现
     if(NULL == this)return;
     travIn(this->lc);
     visit(this->data);
     travIn(this->rc);
 }
+template <typename T> template <typename VST> void BinNode<T>::traIn_I(VST &visit) { // 增加along函数会比较容易想
+    Stack<BinNode<T> *> S;
+    auto x = this;
+    while(true){
+        if(x){ // 技巧，使用函数处理可能更加容易想到
+            S.push(x);
+            x = x->lc;
+        } else if(!S.empty()){
+            x = S.pop();
+            visit(x->data);
+            if(HasRChild(*x))x = x->rc;
+            else x = NULL;
+            // x = x->rc; 邓俊辉的代码，bravo !!!
+        } else
+            break;
+    }
+}
+template <typename T>
+template<typename VST> void BinNode<T>::tranIn_I1(VST &visit) {
+    bool backtracked = false;
+    auto x = this;
+    while(true){
+        if(!backtracked && HasLChild(*x)){
+            x = x->lc;
+        }else{
+            if(NULL == x)break;
+            visit(x->data);
+            if(HasRChild(*x)){
+                x = x->rc;
+                backtracked = false;
+            }
+            else{
+                backtracked = true;
+                x = x->succ();
+            }
+        }
+    }
+}
 template <typename T> void BinNode<T>::alongLeftBranch(BinNode<T> *p, Stack<BinNode<T> *> S) {
     S.push(p);
     while(p->lc)S.push(p = p->lc);
 }
-template <typename T> template <typename VST> void BinNode<T>::travPost(VST &visit) { // 迭代实现不直观，困难得多
+template <typename T> template <typename VST> void BinNode<T>::travPost(VST &visit) {
+    if(NULL == this) return;
+    travPost(lc);travPost(rc);
+    visit(data);
+}
+template <typename T> template <typename VST> void BinNode<T>::travPost_I(VST &visit) { // 迭代实现不直观，困难得多
     Stack<BinNode<T> *> S;
     alongLeftBranch(this, S); // 对以左孩子为根的子树进行中序遍历
     while(!S.empty()){
         auto p = S.top();
-        if(p->rc)alongLeftBranch(p->rc, S); // 对以右孩子为根的子树进行中序遍历
+        if(p->rc)alongLeftBranch(p->rc, S); // 对以右孩子为根的子树进行中序遍历,非邓式方法，比较难想到!!!
         else {
             visit(p->data);
             S.pop();
--- BinaryTree/BinTree.h
+++ BinaryTree/BinTree.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "BinNode.h" //引入二叉树节点类
+#include "../_share/release.h"
 template <typename T> class BinTree { //二叉树模板类
 protected:
    int _size; BinNodePosi(T) _root; //规模、根节点
@@ -23,8 +24,8 @@ public:
    BinNodePosi(T) insertAsRoot ( T const& e ); //插入根节点
    BinNodePosi(T) insertAsLC ( BinNodePosi(T) x, T const& e ); //e作为x的左孩子（原无）插入
    BinNodePosi(T) insertAsRC ( BinNodePosi(T) x, T const& e ); //e作为x的右孩子（原无）插入
-   BinNodePosi(T) attachAsLC ( BinNodePosi(T) x, BinTree<T>* &T ); //T作为x左子树接入
-   BinNodePosi(T) attachAsRC ( BinNodePosi(T) x, BinTree<T>* &T ); //T作为x右子树接入
+   BinNodePosi(T) attachAsLC ( BinNodePosi(T) x, BinTree<T>* &S ); //T作为x左子树接入（原无左子树）
+   BinNodePosi(T) attachAsRC ( BinNodePosi(T) x, BinTree<T>* &S ); //T作为x右子树接入（原无右子树）
    int remove ( BinNodePosi(T) x ); //删除以位置x处节点为根的子树，返回该子树原先的规模
    BinTree<T>* secede ( BinNodePosi(T) x ); //将子树x从当前树中摘除，并将其转换为一棵独立子树
    template <typename VST> //操作器
@@ -44,3 +45,51 @@ public:
    /*DSA*/void stretchToRPath() { stretchByZig ( _root, _size ); } //借助zig旋转，转化为右向单链
 }; //BinTree
 
+template <typename T> int BinTree<T>::updateHeight(BinNode<T> *x) {
+    x->height = 1 + std::max(stature(x->lc), stature(x->rc));
+}
+template <typename T> void BinTree<T>::updateHeightAbove(BinNode<T> *x) {
+    while(x){ updateHeight(x); x = x->parent; }
+}
+template <typename T> BinNode<T> * BinTree<T>::insertAsRoot(const T &e) {
+    _size = 1; return _root = new BinNode<T>(e);
+}
+template <typename T> BinNode<T> * BinTree<T>::insertAsLC(BinNode<T> *x, const T &e) {
+    _size ++; x->insertAsLC(e); updateHeightAbove(x); return x->lc;
+}
+template <typename T> BinNode<T> * BinTree<T>::insertAsRC(BinNode<T> *x, const T &e) {
+    _size ++; x->insertAsLC(e); updateHeightAbove(x);
+    return x->rc;
+}
+template <typename T> BinNode<T> * BinTree<T>::attachAsLC(BinNode<T> *x, BinTree<T> *&S) {
+    if (x->lc = S->_root)x->lc->parent = x;
+    _size += S->_size;updateHeightAbove(x);
+    S->_root == NULL; S->_size = 0; release(S); S = NULL;
+    return x;
+}
+template <typename T> BinNode<T> * BinTree<T>::attachAsRC(BinNode<T> *x, BinTree<T> *&S) {
+    if(x->rc = S->_root)S->_root->parent = x;
+    _size += S->_size; updateHeightAbove(x);
+    S->_root = NULL; S->_size = 0; release(S); S = NULL;
+    return x;
+}
+template <typename T> static int removeAt(BinNode<T> *x){
+    if(NULL == x)return 0;
+    int n = 1 + removeAt(x->lc) + removeAt(x->rc);
+    release(x->data); release(x);
+    return n;
+}
+template <typename T> int BinTree<T>::remove(BinNode<T> *x) {
+    FromParentTo(*x) = NULL;
+    updateHeightAbove(x->parent);
+    int n = removeAt(x);
+    _size -= n;
+    return n;
+}
+template <typename T> BinTree<T>* BinTree<T>::secede(BinNode<T> *x) {
+    FromParentTo(*x) = NULL; updateHeightAbove(x->parent);
+    auto ret = new BinTree<T>();
+    ret->_root = x; ret->_size = x->size(); x->parent=NULL;
+    _size -= ret->_size;
+    return ret;
+}
